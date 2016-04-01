ffa4c20b94822e0f6fb57e8294df7e31764c478a
Михаил Васильев <floyddd-vm@mail.ru>
2016-04-01 14:05:52 +0300

    f

--- smsApp/DB/Insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO contacts (FirstName, SecondName, PhoneNumber)
-VALUES ("Феодосия",  "Третьякова", "111");
-*/
-
-INSERT INTO logs (ContanctID, Date, Time,StatusID)
-VALUES (11,"2016-03-02",  "17:45:09", "5");
-
-/*SELECT * FROM contacts;*/
